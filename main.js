12a2c72c916bc5a996969baf0604daf9d99e2f1b
Lenny <hwilsgaard@gmail.com>
2023-03-12 11:37:42 +0100

    initial

---
+++ main.js
@@ -0,0 +1,46 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    showSpinner();
+
+    const data = new FormData(form);
+
+    const response = await fetch('http://localhost:8080/dream', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        prompt: data.get('prompt'),
+      }),
+    });
+
+    if (response.ok) {
+      const { image } = await response.json();
+      const result = document.querySelector('#result');
+      result.innerHTML = `<img src="${image}" width="512" />`;
+    } else {
+      const err = await response.text();
+      alert(err);
+      console.error(err);
+    }
+    hideSpinner();
+});
+
+function showSpinner() {
+  const button = document.querySelector('button')
+  const explanation = document.querySelector("#result")
+
+  button.disabled = true
+  button.innerHTML = 'Generating... <span class="spinner">🧠</span>';
+  explanation.innerHTML = 'Loading... <span class="spinner">🤔</span>'
+}
+
+function hideSpinner() {
+  const button = document.querySelector('button')
+  const explanation = document.querySelector("#result")
+  
+  button.disabled = false
+  button.innerHTML = 'Generate';
+}
